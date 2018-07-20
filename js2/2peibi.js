1d7ca9f933b314e64874fd5fb86ae42edd2280dd
Joyi <wjywjy123456@163.com>
2018-07-20 21:34:07 +0800

    js2
    至乱序

--- js2/2peibi.js
+++ js2/2peibi.js
@@ -1,16 +1,28 @@
-// 输入框，滑块
+// 输入框值
 var changedNum=document.getElementById("changedNum");
+//滑块值
 var rangeNum=document.getElementById("range");
-
+//滑块加减
 var btn1=document.getElementById("btn1");
 var btn2=document.getElementById("btn2");
-
+//改变输入框值后给滑块
 changedNum.onchange=function () {
     rangeNum.value=changedNum.value;
-    if (changedNum.value < 4 || changedNum.value > 18) {
-        alert("请输入4-18之间的数字");
+    //对输入的数字进行判断,test()检测一个字符串是否匹配reg
+    //符合返回true，没有返回false
+    var reg=/^[0-9]+$/;
+    if (reg.test(changedNum.value)) {
+        if (changedNum.value>=4 && changedNum.value<=18) {
+            return changedNum.value;
+        } else {
+            alert("请输入4-18之间的数字");
+            return false;
+        }
+    } else {
+        alert("请输入整数");
+        return false;
     }
-    // console.log(rangeNum.value);
+    
 }
 rangeNum.onchange=function () {
     changedNum.value=rangeNum.value;   
@@ -25,90 +37,57 @@ btn2.onclick=function () {
 }
 
 //设置杀手，平民数量
-var setBtn=document.getElementById("set");
-var option=document.getElementById("option");
+var setBtn=document.getElementById("set");  //点击设置
+var option=document.getElementById("option");  //获取列表
 
-setBtn.onclick = function () {
-    //实时获得填入的人数
-    var setNum=changedNum.value;
-    //每次点击重置数组对象
-    var killer=new Array();
-    var civilian=new Array();
-    function obj(state,identify) {
-        this.state=state;
-        this.role=identify;
-        this.date='';
-        this.style='';
-    }
-
+setBtn.onclick=function () {
+    var setNum=changedNum.value;    
+    var killerNum=Math.floor(setNum/3);
+    var civilianNum=setNum-killerNum;
     //重置节点
     while (option.hasChildNodes()) {
         option.removeChild(option.firstChild);
     }
-    
-    //根据分配规则在数组中添加对象
-    if (setNum == 4 || setNum == 5) {
-        for (var i = 0; i < 1; i++) {
-            killer[i] = new obj('living', "killer");
-        }
-        for (var i = 0; i < setNum - 1; i++) {
-            civilian[i] = new obj('living', "civilian");
-        }
-    } else if (setNum > 5 && setNum < 9) {
-        for (var i = 0; i < 2; i++) {
-            killer[i] = new obj('living', "killer");
-        }
-        for (var i = 0; i < setNum - 2; i++) {
-            civilian[i] = new obj('living', "civilian");
-        }
-    } else if (setNum >= 9 && setNum <= 11) {
-        for (var i = 0; i < 3; i++) {
-            killer[i] = new obj('living', "killer");
-        }
-        for (var i = 0; i < setNum - 3; i++) {
-            civilian[i] = new obj('living', "civilian");
-        }
-    } else if (setNum >= 12 && setNum <= 15) {
-        for (var i = 0; i < 4; i++) {
-            killer[i] = new obj('living', "killer");
-        }
-        for (var i = 0; i < setNum - 4; i++) {
-            civilian[i] = new obj('living', "civilian");
-        }
-    } else if (setNum >= 16 && setNum <= 18) {
-        for (var i = 0; i < 5; i++) {
-            killer[i] = new obj('living', "killer");
-        }
-        for (var i = 0; i < setNum - 5; i++) {
-            civilian[i] = new obj('living', "civilian");
-        }
-    }
-    
-    var sumArr=killer.concat(civilian);
 
-    //数组乱序
-    realArr=Array.prototype.slice.call(sumArr,0);
-    var sumRandom=new Array();
-    for(i=0;i<sumArr.length;i++){
-        var num=Math.floor(Math.random()*realArr.length);
-        sumRandom.push(realArr[num]);
-        realArr.splice(num,1);
+    //添加杀手
+    for(var k=1;k<=killerNum;k++){
+        var lis=document.createElement('li'); 
+        option.appendChild(lis).innerText="杀手"+1+"人";
+        lis.style.color = '#29bde0';
     }
-    
-    // 存储数据
-    sessionStorage.sumRandom = JSON.stringify(sumRandom); 
-    
-    //实现随机分配
-    for (i = 0; i < sumRandom.length; i++) {
-        var li = document.createElement('li');
-        if (sumRandom[i].role == 'killer') {
-            option.appendChild(li);
-            li.innerHTML = "杀手 1 人";
-            li.style.color = '#29bde0';
-        } else if (sumRandom[i].role == 'civilian') {
-            option.appendChild(li);
-            li.innerHTML = "平民 1 人";
-            li.style.color = '#fbb435';
-        }
+    for(var c=1;c<=civilianNum;c++){
+        var lis=document.createElement('li'); 
+        option.appendChild(lis).innerText="平民"+1+"人";
+        lis.style.color = '#fbb435';
     }
+    //添加li的id
+    var liId=document.getElementsByTagName('li');
+    for (var i = 0; i <liId.length; i++) {
+        liId[i].id='li'+i;
+    }
+    //获取li将其变成数组存储到liArr中
+    var liArr=[];
+    for(var l=0;l<liId.length;l++){
+        liArr.push(liId[l]);
+    }
+
+    console.log(liArr);
+    //乱序排列liArr
+    var result=[];
+    // for(var r=0;r<setNum;r++){
+    //     var ran=Math.floor(Math.random()*setNum);
+    //     console.log(ran);
+    //     result.push(liArr.splice(ran,1)[0]);
+    //     // console.log(result);
+    // };
+    for(var r=0;r<setNum;r++){
+        var ran=Math.floor(Math.random()*(liArr.length-r));
+        result.push(liArr[ran]);
+        liArr[ran]=liArr[liArr.length-r-1];
+    }
+    console.log(result);
+
+
+    //将乱序后的数组添加到原数组中
+
 }
